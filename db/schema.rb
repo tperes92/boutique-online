1ccf0bff47b4d9ec75cae2e5563dcd663b91c440
Claire Dubas <dub.claire@icloud.com>
2020-03-10 12:57:17 +0100

    seed + images

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,62 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_03_09_185951) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cart_items", force: :cascade do |t|
+    t.bigint "cart_id"
+    t.bigint "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_id"], name: "index_cart_items_on_cart_id"
+    t.index ["item_id"], name: "index_cart_items_on_item_id"
+  end
+
+  create_table "carts", force: :cascade do |t|
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_carts_on_user_id"
+  end
+
+  create_table "item_orders", force: :cascade do |t|
+    t.bigint "order_id"
+    t.bigint "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_item_orders_on_item_id"
+    t.index ["order_id"], name: "index_item_orders_on_order_id"
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "description"
+    t.decimal "price"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,63 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Item1 = Item.create(title: 'Persan', description: 'Un chat', price: rand(5..100), image: 'chat.001.jpeg')
+Item1.save
+
+Item2 = Item.create(title: 'Bengal', description: 'Un chat', price: rand(5..100), image: 'chat.002.jpeg')
+Item2.save
+
+Item3 = Item.create(title: 'Main Coon', description: 'Un chat', price: rand(5..100), image: 'chat.003.jpeg')
+Item3.save
+
+Item4 = Item.create(title: 'Siamois', description: 'Un chat', price: rand(5..100), image: 'chat.004.jpeg')
+Item4.save
+
+Item5 = Item.create(title: 'Sphynx', description: 'Un chat', price: rand(5..100), image: 'chat.005.jpeg')
+Item5.save
+
+Item6 = Item.create(title: 'Radgoll', description: 'Un chat', price: rand(5..100), image: 'chat.006.jpeg')
+Item6.save
+
+Item7 = Item.create(title: 'Norvégien', description: 'Un chat', price: rand(5..100), image: 'chat.007.jpeg')
+Item7.save
+
+Item8 = Item.create(title: 'Savannah', description: 'Un chat', price: rand(5..100), image: 'chat.008.jpeg')
+Item8.save
+
+Item9 = Item.create(title: 'Sacré de Birmanie', description: 'Un chat', price: rand(5..100), image: 'chat.009.jpeg')
+Item9.save
+
+Item10 = Item.create(title: 'Chartreux', description: 'Un chat', price: rand(5..100), image: 'chat.010.jpeg')
+Item10.save
+
+Item11 = Item.create(title: 'Sibérien', description: 'Un chat', price: rand(5..100), image: 'chat.011.jpeg')
+Item11.save
+
+Item12 = Item.create(title: 'Himalayen', description: 'Un chat', price: rand(5..100), image: 'chat.012.jpeg')
+Item12.save
+
+Item13 = Item.create(title: 'Bombay', description: 'Un chat', price: rand(5..100), image: 'chat.013.jpeg')
+Item13.save
+
+Item14 = Item.create(title: 'Abyssin', description: 'Un chat', price: rand(5..100), image: 'chat.014.jpeg')
+Item14.save
+
+Item15 = Item.create(title: 'Toyger', description: 'Un chat', price: rand(5..100), image: 'chat.015.jpeg')
+Item15.save
+
+Item16 = Item.create(title: 'Ragamuffin', description: 'Un chat', price: rand(5..100), image: 'chat.016.jpeg')
+Item16.save
+
+Item17 = Item.create(title: 'Balinais', description: 'Un chat', price: rand(5..100), image: 'chat.017.jpeg')
+Item17.save
+
+Item18 = Item.create(title: 'Manx', description: 'Un chat', price: rand(5..100), image: 'chat.018.jpeg')
+Item18.save
+
+Item19 = Item.create(title: 'Somali', description: 'Un chat', price: rand(5..100), image: 'chat.019.jpeg')
+Item19.save
+
+Item20 = Item.create(title: 'Thaï', description: 'Un chat', price: rand(5..100), image: 'chat.020.jpeg')
+Item20.save
