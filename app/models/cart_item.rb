9b4a78aa223a23fa8f1c7c92de3e9d428687e8b1
Claire Dubas <dub.claire@icloud.com>
2020-03-11 13:52:13 +0100

    cart4

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,8 +1,8 @@
 class CartItem < ApplicationRecord
-	belongs_to :item
-	belongs_to :cart
+	belongs_to :item, optional:true
+	belongs_to :cart, optional:true
 
 	def subtotal
-		quantity * item.price
+		quantity * item.original_price
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
  resources :items
  resources :carts
  resources :cart_items
+
  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
