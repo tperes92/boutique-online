51309681e82d539e5432beb6d27e5a97fde9db99
GabTHP <gabriel.bouakira@hotmail.fr>
2020-03-11 14:51:04 +0100

    Connexion et déconnexion session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+
+	 layout :set_layout
+ def set_layout
+    if current_user
+      'profil'
+    else
+      'application'
+    end
+ end
 end
