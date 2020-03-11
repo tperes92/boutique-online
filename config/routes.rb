cbdfbe52e4902a091b9c480eefeaec2d5572519e
Claire Dubas <dub.claire@icloud.com>
2020-03-11 14:03:40 +0100

    cart7

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root 'items#index'  
- devise_for :users
- resources :items
- resources :carts
- resources :cart_items
+	devise_for :users
+	resources :items
+  resources :carts
+  resources :cart_items
 
- # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
