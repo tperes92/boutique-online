d52708c82b6c183ec20aa08ccf66a830ce5cf92d
Claire Dubas <dub.claire@icloud.com>
2020-03-11 13:34:24 +0100

    routes + cart

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'items#index'  
+  get '/cart', to: 'cart#show'
  devise_for :users
  resources :items
- get '/cart', to: 'cart#show'
+ resources :carts
+ resources :cart_items
  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
