af18571ce95ebf0704f92f07b0cbe426a489e4b3
Nathan Baliteau <nathanbaliteau@gmail.com>
2020-03-12 18:55:40 +0100

    stripe advance

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,22 +4,32 @@ class OrdersController < ApplicationController
 
   def create
     # Amount in cents
-    @amount = 500
+    @amount = 0
+    @order = Order.new(user: current_user, cart: current_user.cart)
+    @order.items.each do |item|
+      @amount += item.price
+    end
+    @order.amount = @amount
 
-    customer = Stripe::Customer.create({
-      email: params[:stripeEmail],
-      source: params[:stripeToken],
-    })
+    if @order.save
 
-    charge = Stripe::Charge.create({
-      customer: customer.id,
-      amount: @amount,
-      description: 'Paiement Cat Eye',
-      currency: 'eur',
-    })
+      customer = Stripe::Customer.create({
+        email: params[:stripeEmail],
+        source: params[:stripeToken],
+      })
 
-  rescue Stripe::CardError => e
-    flash[:error] = e.message
-    redirect_to new_charge_path
-  end
+      charge = Stripe::Charge.create({
+        customer: customer.id,
+        amount: @amount * 100,
+        description: 'Paiement Cat Eye',
+        currency: 'eur',
+      })
+
+    end
+
+    rescue Stripe::CardError => e
+      flash[:error] = e.message
+      redirect_to new_order_path
+    end
+
 end
