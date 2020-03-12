85f0995252085377781dce0f21e1e60f92d4db64
GabTHP <gabriel.bouakira@hotmail.fr>
2020-03-12 16:08:46 +0100

    modif seed photo fonctionnelle en prod

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,34 +6,34 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Item1 = Item.create(title: 'Persan', description: 'Un chat', price: rand(5..100), image: "chat1.jpg")
+Item1 = Item.create(title: 'Baby', description: 'Un chat', price: rand(5..100), image: "chat1.jpg")
 Item1.save
 
 
-Item12 = Item.create(title: 'Persan', description: 'Un chat', price: rand(5..100), image: "chat12.jpg")
+Item12 = Item.create(title: 'Himalayen', description: 'Un chat', price: rand(5..100), image: "chat12.jpg")
 Item1.save
 
 
-Item13 = Item.create(title: 'Persan', description: 'Un chat', price: rand(5..100), image: "chat13.jpg")
+Item13 = Item.create(title: 'Bombay', description: 'Un chat', price: rand(5..100), image: "chat13.jpg")
 Item1.save
 
-Item14 = Item.create(title: 'Persan', description: 'Un chat', price: rand(5..100), image: "chat14.jpg")
+Item14 = Item.create(title: 'Abyssin', description: 'Un chat', price: rand(5..100), image: "chat14.jpg")
 Item1.save
 
-Item15 = Item.create(title: 'Persan', description: 'Un chat', price: rand(5..100), image: "chat15.jpg")
+Item15 = Item.create(title: 'Toyger', description: 'Un chat', price: rand(5..100), image: "chat15.jpg")
 Item1.save
 
-Item16 = Item.create(title: 'Persan', description: 'Un chat', price: rand(5..100), image: "chat16.jpg")
+Item16 = Item.create(title: 'Ragamuffin', description: 'Un chat', price: rand(5..100), image: "chat16.jpg")
 Item1.save
 
-Item17 = Item.create(title: 'Persan', description: 'Un chat', price: rand(5..100), image: "chat17.jpg")
+Item17 = Item.create(title: 'Balinais', description: 'Un chat', price: rand(5..100), image: "chat17.jpg")
 Item1.save
 
-Item18 = Item.create(title: 'Persan', description: 'Un chat', price: rand(5..100), image: "chat18.jpg")
+Item18 = Item.create(title: 'Manx', description: 'Un chat', price: rand(5..100), image: "chat18.jpg")
 Item1.save
 
-Item19 = Item.create(title: 'Persan', description: 'Un chat', price: rand(5..100), image: "chat19.jpg")
+Item19 = Item.create(title: 'Somali', description: 'Un chat', price: rand(5..100), image: "chat19.jpg")
 Item1.save
 
-Item20 = Item.create(title: 'Persan', description: 'Un chat', price: rand(5..100), image: "chat20.jpg")
+Item20 = Item.create(title: 'Thaï', description: 'Un chat', price: rand(5..100), image: "chat20.jpg")
 Item1.save
