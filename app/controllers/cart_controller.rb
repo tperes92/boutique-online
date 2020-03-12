0e5b833660154d6d6ea8b9a269094afb5141c4b7
GabTHP <gabriel.bouakira@hotmail.fr>
2020-03-12 11:12:37 +0100

    Fonctionalité panier ok, page à show à restructurer

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -2,9 +2,9 @@ class CartController < ApplicationController
      after_action :authenticate_user!
 
   def show
-    @cart = Cart.find_by(id: current_user)
+    @cart = Cart.find_by(user: current_user)
       if @cart.items != nil
-        @cart_items = CartItem.all.select {|cart_item| cart_item.cart.id == @cart.id}
+        @cart_items = CartItem.all.select {|cart_item| cart_item.cart == @cart}
       else 
         redirect_to root_path
         flash[:alert] = "Votre panier est tellement vide.. vous devriez le remplir, entre vous et moi"
